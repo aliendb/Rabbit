549ac1841b75c69f8448c4610533c9a3394ecb13
D <12778164+aliendb@users.noreply.github.com>
2017-12-17 00:05:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const http = require('http');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${ip}:${port}/`);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
