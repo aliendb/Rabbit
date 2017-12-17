36d4ae05a6f94236ed41c891b73dfb299f7bdd5b
D <12778164+aliendb@users.noreply.github.com>
2017-12-16 19:56:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
